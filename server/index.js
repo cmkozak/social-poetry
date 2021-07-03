94e71a08409791c6b0815e35fea25857f75718e6
Michael Kozak <charlesmichaelk@gmail.com>
2021-07-03 12:11:43 -0500

    init

---
+++ server/index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const PORT = process.env.PORT || 4000;
+const morgan = require("morgan");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const config = require("./config/db");
+const app = express();
+
+mongoose.set("useCreateIndex", true);
+mongoose
+  .connect(config.database, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("Database is connected");
+  })
+  .catch((err) => {
+    console.log({ database_error: err });
+  });
+
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(morgan("dev"));
+
+const userRoutes = require("./api/user/route/user");
+app.use("/user", userRoutes);
+
+app.listen(PORT, () => {
+  console.log(`App is running on ${PORT}`);
+});
