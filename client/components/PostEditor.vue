d4f9e22677466fa19e70a5b503654f6e54df0d4f
Michael Kozak <charlesmichaelk@gmail.com>
2021-09-04 16:26:30 -0500

    add rich text editing

--- client/components/PostEditor.vue
+++ client/components/PostEditor.vue
@@ -12,9 +12,77 @@
       <div v-if="!$v.postTitle.minLength && submitted" class="error">
         Title must be longer than 4 characters
       </div>
-      <editor-content :editor="editor" />
-      <div v-if="!$v.postContent.required && submitted" class="error">
-        Conent must not be blank
+      <div v-if="editor" class="editor-buttons">
+        <button
+          :class="{ 'is-active': editor.isActive('heading', { level: 1 }) }"
+          @click.prevent="
+            editor.chain().focus().toggleHeading({ level: 1 }).run()
+          "
+        >
+          H1
+        </button>
+        <button
+          :class="{ 'is-active': editor.isActive('heading', { level: 2 }) }"
+          @click.prevent="
+            editor.chain().focus().toggleHeading({ level: 2 }).run()
+          "
+        >
+          H2
+        </button>
+        <button
+          :class="{ 'is-active': editor.isActive('heading', { level: 3 }) }"
+          @click.prevent="
+            editor.chain().focus().toggleHeading({ level: 3 }).run()
+          "
+        >
+          H3
+        </button>
+        <button
+          :class="{ 'is-active': editor.isActive('paragraph') }"
+          @click.prevent="editor.chain().focus().setParagraph().run()"
+        >
+          Paragraph
+        </button>
+        <button
+          :class="{ 'is-active': editor.isActive('bold') }"
+          @click.prevent="editor.chain().focus().toggleBold().run()"
+        >
+          Bold
+        </button>
+        <button
+          :class="{ 'is-active': editor.isActive('italic') }"
+          @click.prevent="editor.chain().focus().toggleItalic().run()"
+        >
+          Italic
+        </button>
+        <button
+          :class="{ 'is-active': editor.isActive('strike') }"
+          @click.prevent="editor.chain().focus().toggleStrike().run()"
+        >
+          Strike
+        </button>
+        <button
+          :class="{ 'is-active': editor.isActive({ textAlign: 'left' }) }"
+          @click.prevent="editor.chain().focus().setTextAlign('left').run()"
+        >
+          Left
+        </button>
+        <button
+          :class="{ 'is-active': editor.isActive({ textAlign: 'center' }) }"
+          @click.prevent="editor.chain().focus().setTextAlign('center').run()"
+        >
+          Center
+        </button>
+        <button
+          :class="{ 'is-active': editor.isActive({ textAlign: 'right' }) }"
+          @click.prevent="editor.chain().focus().setTextAlign('right').run()"
+        >
+          Right
+        </button>
+      </div>
+      <editor-content :editor="editor" class="editor" />
+      <div v-if="!$v.postContent.editorContent && submitted" class="error">
+        Content must not be blank
       </div>
     </div>
     <button class="submit" type="submit">Submit</button>
@@ -24,6 +92,7 @@
 <script>
 import { Editor, EditorContent } from '@tiptap/vue-2';
 import StarterKit from '@tiptap/starter-kit';
+import TextAlign from '@tiptap/extension-text-align';
 import { required, minLength, maxLength } from 'vuelidate/lib/validators';
 
 export default {
@@ -66,7 +135,12 @@ export default {
   },
   mounted() {
     this.editor = new Editor({
-      extensions: [StarterKit],
+      extensions: [
+        StarterKit,
+        TextAlign.configure({
+          types: ['heading', 'paragraph'],
+        }),
+      ],
       content: this.postContent,
     });
   },
@@ -140,5 +214,24 @@ export default {
   font-size: 14px;
   margin-top: 2px;
   color: red;
+}
+
+.editor-buttons {
+  margin: 10px 0px;
+}
+
+.editor {
+  text-align: left;
+}
+</style>
+
+<style lang="scss">
+.ProseMirror {
+  height: 500px;
+  padding: 7px;
+  outline: -webkit-focus-ring-color auto 1px;
+  p {
+    margin-top: 0;
+  }
 }
 </style>
